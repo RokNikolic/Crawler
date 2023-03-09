6df357cd24fdd56aea83eba12059c32d9a8e12af
Rok Nikolič <rok.nikolic@gmail.com>
2023-03-09 01:44:12 +0100

    added some basic skeleton functions

--- crawler_main.py
+++ crawler_main.py
@@ -1,3 +1,60 @@
+import asyncio
+import html.parser
+
+
+class UrlParser(html.parser.HTMLParser):
+    def __init__(self, base: str,):
+        super().__init__()
+        self.base = base
+        self.links = set()
+
+    def handle_starttag(self, tag: str, attrs):
+        # look for <a href="...">
+        if tag != "a":
+            return
+
+        for attr, url in attrs:
+            if attr != "href":
+                continue
+
+            #if (url := self.filter_url(self.base, url)) is not None:
+            else:
+                self.links.add(url)
+
+frontier = []
+crawled_urls = set()
+
+def get_frontier_url():
+    task = frontier[0]
+    frontier.pop(0)
+    return task
+
+def add_to_frontier(url):
+    if not url in crawled_urls:
+        frontier.append(url)
+
+
+class Crawler:
+    def __init__(self, frontier) -> None:
+        self.frontier = frontier
+        
+
+    def worker(self):
+        while True:
+            try:
+                self.process_one()
+            except asyncio.CancelledError:
+                return
+
+    def parse(self, url):
+        pass
+
+    def crawl(self):
+        pass
+
+
+
+
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import asyncio
+import html.parser
 
 
 async def print1(string: str):
@@ -15,5 +16,25 @@ async def main():
     done, pending = await asyncio.wait(tasks)
 
 
+class UrlParser(html.parser.HTMLParser):
+    def __init__(self, base: str,):
+        super().__init__()
+        self.base = base
+        self.links = set()
+
+    def handle_starttag(self, tag: str, attrs):
+        # look for <a href="...">
+        if tag != "a":
+            return None
+
+        for attr, url in attrs:
+            if attr != "href":
+                continue
+
+            #if (url := self.filter_url(self.base, url)) is not None:
+            else:
+                self.links.add(url)
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    #asyncio.run(main())
