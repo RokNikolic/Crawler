221340602d2b3f2376902346cf2bd7d2003a9025
MatejBevec <matejbevec98@gmail.com>
2023-03-11 21:19:39 +0100

    Added sitemap handling, site and page data parsing

--- crawler_main.py
+++ crawler_main.py
@@ -5,6 +5,7 @@ import threading
 import requests
 from urllib.parse import urljoin, urlparse
 from urllib.robotparser import RobotFileParser
+import traceback
 
 import bs4
 
@@ -34,34 +35,49 @@ def add_to_frontier(url):
         frontier.put(url)
 
 
-def request(url):
-    """Fetches page at url and returns HTML content."""
+def request_page(url):
+    """Fetches page at url and returns HTML content and metadata as dict."""
 
     # TODO: Don't request if outside given domains?
 
-    # TODO: Add correct sitemap handling based on the current domain.
-
     domain = urlparse(url).netloc
+    site_data = None    # Parsed SITE metadata
+    page_raw = None     # Raw PAGE content and metadata
 
     # If not enough time has elapsed since last request, return url to end of queue
     if domain in domain_ips:
         ip = domain_ips[domain]
         since_last_req = time.time() - ip_last_visits[ip]
-
         robots_delay = domain_rules[domain].crawl_delay(USERAGENT)
         min_delay = robots_delay if robots_delay is not None else 5
         if since_last_req < min_delay:
             add_to_frontier(url)
-            return None
+            return None, None
+        
     else:
         # Save robots.txt rules for new domain
         rp = RobotFileParser()
-        rp.set_url(urljoin(url, domain) + "/robots.txt")
+        robots_url = urljoin(url, domain) + "/robots.txt"
+        rp.set_url(robots_url)
         rp.read()
         domain_rules[domain] = rp
 
+        # Make site info dict with robots.txt and sitemap contents
+        robots_content = requests.get(robots_url).text
+        sitemap_urls = rp.site_maps()
+        if sitemap_urls is None:
+            sitemap_content = None
+        else:
+            sitemap_content = requests.get(sitemap_urls[0]).text
+        site_data = {
+            "domain": domain,
+            "robots": robots_content,
+            "sitemap": sitemap_content
+        }
+
+    # If URL is dissalowed by robots.txt, don't fetch
     if not domain_rules[domain].can_fetch(USERAGENT, url):
-        return
+        return None, site_data
         
     # Make a GET request
     print("Fetching ", url)
@@ -69,58 +85,89 @@ def request(url):
     response = requests.get(url, stream=True)
 
     # Save server IP and request time
-    ip = response.raw._connection.sock.getsockname()
-    if domain not in domain_ips:
-        domain_ips[domain] = ip
-    ip_last_visits[ip] = req_time
+    if response.raw._connection.sock: # HACK
+        ip = response.raw._connection.sock.getsockname()
+        if domain not in domain_ips:
+            domain_ips[domain] = ip
+        ip_last_visits[ip] = req_time
 
-    # Check response validity
+    # Make raw page content and info dict 
     if response.ok and len(response.content) > 0:
-        return response.text
-    else:
-        return None
+        page_raw = {
+            "html_content": response.text,
+            "domain": domain,
+            "url": url,
+            "accessed_time": req_time
+        }
 
     # TODO: Perform Selenium parsing if we suspect dynamic content.
 
+    return page_raw, site_data
 
-def parse(html, base_url):
+
+def parse_page(page_raw, base_url):
     """Parses HTML content and extract links (urls)."""
 
     # TODO: Detect duplicate pages based on content or hash.
 
     # TODO: If URL already parsed we must still add the link connection to DB.
 
-    if html is None: return None
+    if page_raw is None: return None
 
     # Parse HTML and extract links
-    soup = bs4.BeautifulSoup(html, 'html.parser')
+    soup = bs4.BeautifulSoup(page_raw["html_content"], 'html.parser')
 
+    # Basic page info
+    page_info = page_raw
+    page_info["page_type_code"] = None # TODO
+    
     # Find the urls in page
     links = []
     for link in soup.select('a'):
         found_link = link.get('href')
-        links.append(urljoin(base_url, found_link))
+        to = urljoin(base_url, found_link)
+        links.append({"from_page": base_url, "to_page": to})
+
+    # Find the images in page (<img> tags)
+    imgs = []
+    for img in soup.select('img'):
+        src = img.get('src')
+        if src is not None:
+            src_full = urljoin(base_url, src)
+            img_info = {
+                "filename": os.path.basename(src_full),
+                "content_type": None, # TODO: ?
+                "data": None,
+                "accessed_time": None
+            } 
+            imgs.append(img_info)
+
+    # TODO: Page data?
 
     # TODO: Also include links from onclick events
 
-    # TODO: Detect images based on img tag with src containing URL
+    page_data = {
+        "info": page_info,
+        "urls": links,
+        "imgs": imgs
+    }
 
-    return links
+    return page_data
 
 
-def save(data, url, conn):
-    """Saves parsed page content to DB."""
+def save_to_db(page_data, site_data, conn):
+    """Saves parsed site and page data to DB."""
 
-    if data is None: return
+    if site_data is not None:
+        DBManager.insert_site(conn, site_data)
 
-    for link in data:
-        add_to_frontier(link)
-
-    site_json = {"domain": url}
-    DBManager.insert_site(conn, site_json)
-    # THROWS: "robots" ?
-
-    # TODO: insert parsed data 
+    if page_data is not None:
+        # Add found urls to frontier (TODO: should this be here?)
+        for url in page_data["urls"]:
+            add_to_frontier(url["to_page"])
+        # TODO: Should implement something like:
+        # DBManager.insert_page_all(conn, info, urls, imgs)
+        pass
 
 
 class Crawler(threading.Thread):
@@ -137,10 +184,9 @@ class Crawler(threading.Thread):
         """Fetches, parses and saves next page from frontier."""
 
         url = get_url_from_frontier()
-        html = request(url)
-        data = parse(html, url)
-        save(data, url, self.conn)
-        pass
+        page_raw, site_data = request_page(url)
+        page_data = parse_page(page_raw, url)
+        save_to_db(page_data, site_data, self.conn)
 
     def run(self):
         """Continuously processes pages from frontier."""
@@ -149,9 +195,8 @@ class Crawler(threading.Thread):
             try:
                 self.process_next()
             except Exception as e:
-                print(e)
+                print(traceback.format_exc())
                 # TODO
-                pass
 
 
 if __name__ == '__main__':
@@ -165,7 +210,7 @@ if __name__ == '__main__':
     for url in frontier.queue:
         domain_rules[url] = None
 
-    NTHREADS = 2
+    NTHREADS = 1
 
     db_manager = DBManager()
 
@@ -176,10 +221,11 @@ if __name__ == '__main__':
         crawler.start()
 
     while True:
-        try:
-            time.sleep(1)
-        except Exception as e:
-            print(e)
-            print(frontier)
-            print(ip_last_visits)
-            exit()
+        time.sleep(1)
+        # try:
+        #     time.sleep(1)
+        # except Exception as e:
+        #     print(e)
+        #     print(frontier)
+        #     print(ip_last_visits)
+        #     exit()
